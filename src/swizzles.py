42a17b00b00ebc25288558acc2e9e46c92d930e7
Feles007 <75643325+Feles007@users.noreply.github.com>
2025-01-02 19:31:48 -0800

    Update swizzles generator for new structure

--- src/swizzles.py
+++ src/swizzles.py
@@ -1,32 +1,30 @@
 import itertools
 
 
-print("// python3 src/swizzles.py > include/fstd/vector/swizzles.hpp")
+print("// python3 src/swizzles.py > include/fstd/types/vector/swizzles.hpp")
 
-def f(chars, li):
-	s = "".join(li)
-	li = [str(chars.index(x)) for x in li]
-	array = ", ".join(li)
-	print(f"using {s} = Swizzle<{array}>;")
+def print_swizzle(buffer: list, chars: list):
+	name = "".join(buffer)
+	indices = ", ".join([str(chars.index(x)) for x in buffer])
+	print(f"using {name} = Swizzle<{indices}>;")
 
-def g(chars):
-	for a in chars:
-		f(chars, [a])
-	
-	for a in chars:
-		for b in chars:
-			f(chars, [a, b])
+def swizzle_spot(depth: int, buffer: list, chars: list):
+	for char in chars:
+		buffer[len(buffer) - depth - 1] = char
+		if depth == 0:
+			print_swizzle(buffer, chars)
+		else:
+			swizzle_spot(depth - 1, buffer, chars)
 
-	for a in chars:
-		for b in chars:
-			for c in chars:
-				f(chars, [a, b, c])
+def run_once(chars: list, target_length: int):
+	buffer = [' ' for _ in range(target_length)]
+	swizzle_spot(target_length - 1, buffer, chars)
 
-	for a in chars:
-		for b in chars:
-			for c in chars:
-				for d in chars:
-					f(chars, [a, b, c, d])
+def run(chars: str, target_length: int):
+	chars = [c for c in chars]
+	for i in range(1, target_length + 1):
+		run_once(chars, i)
 
-g("xyzw")
-g("rgba")
+
+run("xyzw", 4)
+run("rgba", 4)
