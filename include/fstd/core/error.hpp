1bc384a2f94a8c995c3fd4e92f8b44044739b8c7
Feles007 <75643325+Feles007@users.noreply.github.com>
2025-03-02 13:26:00 -0600

    Allow formatting in panic

--- include/fstd/core/error.hpp
+++ include/fstd/core/error.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
-#include "num.hpp"
 #include <cstdio>
 #include <cstdlib>
 
-#define panic(message) unrecoverable_error(message, __FILE__, __LINE__)
+#define panic(fmt, ...)                              \
+	do {                                             \
+		std::printf(fmt __VA_OPT__(, ) __VA_ARGS__); \
+		std::exit(1);                                \
+	} while (0)
 
 #define fassert(expression) \
 	if (!(expression)) panic("Assert (" #expression ") failed")
@@ -14,8 +17,3 @@
 #else
 	#define debug_fassert(expression) fassert(expression)
 #endif
-
-[[noreturn]] inline void unrecoverable_error(const char *message, const char *file, usize line) {
-	std::printf("%s - %s:%zu\n", message, file, line);
-	std::exit(1);
-}
