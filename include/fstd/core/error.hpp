ea60238e91eeb6f476c1f4d4e03cbc2da0095c6e
Feles007 <75643325+Feles007@users.noreply.github.com>
2025-03-02 16:01:44 -0600

    Add uX and iX formatting macros

--- include/fstd/core/error.hpp
+++ include/fstd/core/error.hpp
@@ -1,12 +1,13 @@
 #pragma once
 
+#include "fmt.hpp"
 #include <cstdio>
 #include <cstdlib>
 
-#define panic(fmt, ...)                              \
-	do {                                             \
-		std::printf(fmt __VA_OPT__(, ) __VA_ARGS__); \
-		std::exit(1);                                \
+#define panic(fmt, ...)                                                                          \
+	do {                                                                                         \
+		std::printf(fmt " - %s:" FMT_USIZE "\n", __FILE__, __LINE__ __VA_OPT__(, ) __VA_ARGS__); \
+		std::exit(1);                                                                            \
 	} while (0)
 
 #define fassert(expression) \
---
+++ include/fstd/core/fmt.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cinttypes>
+
+#define FMT_U8  "%" PRIu8
+#define FMT_U16 "%" PRIu16
+#define FMT_U32 "%" PRIu32
+#define FMT_U64 "%" PRIu64
+
+#define FMT_I8  "%" PRIi8
+#define FMT_I16 "%" PRIi16
+#define FMT_I32 "%" PRIi32
+#define FMT_I64 "%" PRIi64
+
+#define FMT_USIZE "%" PRIuPTR
+#define FMT_ISIZE "%" PRIiPTR
